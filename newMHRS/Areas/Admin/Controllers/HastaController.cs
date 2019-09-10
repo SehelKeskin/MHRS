23b1106b575c99b164b4b0c986a65d7deb89cd4f
sehel keskin <sehelcigdemkeskin@hotmail.com>
2019-09-10 23:37:17 +0300

    Popup Sorunu Halledildi. Menü çalışmaları devam ediyor.

--- newMHRS/Areas/Admin/Controllers/HastaController.cs
+++ newMHRS/Areas/Admin/Controllers/HastaController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using newMHRS;
 using newMHRS.Areas.Admin.Models;
 using newMHRS.Models;
@@ -23,6 +24,8 @@ namespace newMHRS.Areas.Admin.Controllers
         {
             return View(db.Hastas.ToList());
         }
+
+
 
         // GET: Admin/Hasta/Details/5
         public ActionResult Details(int? id)
